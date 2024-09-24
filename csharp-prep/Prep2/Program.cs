fded9da9aadb5a09acaf7ff25efb975262f1809b
LetThereBeLightMode </usr/bin/python3>
2024-09-24 11:50:33 -0700

    Refactor program to include grade calculation and pass/fail status

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,26 +6,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your grade?");
-        int grade = Console.ReadLine();
+        grade = int.Console.ReadLine();
+        
         Console.WriteLine("you endered: " + grade + " as your grade" + ": this correlates to a letter grade of: " + GetLetterGrade(grade));
-
+        if grade >= 70
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed!");
+        }
     }
 
     static string GetLetterGrade(int grade)
     {
-        if grade >= 90
+        if (grade >= 90)
         {
             return "A";
         }
-        else if grade >= 80
+        else if (grade >= 80)
         {
             return "B";
         }
-        else if grade >= 70
+        else if (grade >= 70)
         {
             return "C";
         }
-        else if grade >= 60
+        else if (grade >= 60)
         {
             return "D";
         }
@@ -33,7 +41,6 @@ class Program
         {
             return "F";
         }
-            
     }
 }
 
