336229d4d5895e2d1dbd7d144217601272b1db36
LetThereBeLightMode </usr/bin/python3>
2024-09-24 11:57:13 -0700

    Refactor program to include grade calculation and input validation

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,10 +6,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your grade?");
-        grade = int.Console.ReadLine();
+        int grade = int.Parse(Console.ReadLine());
         
         Console.WriteLine("you endered: " + grade + " as your grade" + ": this correlates to a letter grade of: " + GetLetterGrade(grade));
-        if grade >= 70
+        if (grade >= 70)
         {
             Console.WriteLine("You passed!");
         }
