b1b91732d979bb7dd28167244068027527cf520c
LetThereBeLightMode </usr/bin/python3>
2024-09-24 11:41:43 -0700

    Refactor program to include grade calculation

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Text.RegularExpressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade?");
+        int grade = Console.ReadLine();
+        Console.WriteLine("you endered: " + grade + " as your grade" + ": this correlates to a letter grade of: " + GetLetterGrade(grade));
+
+    }
+
+    static string GetLetterGrade(int grade)
+    {
+        if grade >= 90
+        {
+            return "A";
+        }
+        else if grade >= 80
+        {
+            return "B";
+        }
+        else if grade >= 70
+        {
+            return "C";
+        }
+        else if grade >= 60
+        {
+            return "D";
+        }
+        else
+        {
+            return "F";
+        }
+            
     }
 }
+
