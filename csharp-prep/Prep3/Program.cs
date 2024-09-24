008138d81d7552ed9efa82fb8632ea8197c1d024
LetThereBeLightMode </usr/bin/python3>
2024-09-24 11:58:58 -0700

    Refactor program to include number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        Console.WriteLine("I'm thinking of a number between 1 and 10. Can you guess what it is?");
+        int guess = int.Parse(Console.ReadLine());
+        while (guess != number)
+        {
+            Console.WriteLine("You guessed incorrectly. guess again!");
+            guess = int.Parse(Console.ReadLine());
+        if (guess > number)    
+            {
+                Console.WriteLine("Guess lower!");
+            }
+            else
+            {
+                Console.WriteLine("Guess higher!");
+            }
+            }
+}
 }
