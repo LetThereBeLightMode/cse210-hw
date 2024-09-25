54fde4ab05bc9ae4e9726d4644d74362fcdf8496
LetThereBeLightMode <wel24010@byui.edu>
2024-09-25 13:16:24 -0700

    Refactor program to include user input for a list of numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,5 +7,14 @@ class Program
     static void Main(string[] args)
     {
         List<int> numbers = new List<int>();
+    int number; 
+    Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do 
+        {
+            numbers.Add(number);
+            Console.WriteLine("Enter another number:");
+            number = int.Parse(Console.ReadLine());
+        }
+        while (number != 0);
     }
 }
