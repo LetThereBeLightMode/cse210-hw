0bfdd44970b620935ce1aae012e08bc9311a9cac
LetThereBeLightMode <wel24010@byui.edu>
2024-09-25 13:34:43 -0700

    Refactor program to include user input for username and prompt for a number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,4 +6,21 @@ class Program
     {
         Console.WriteLine("Hello Prep5 World!");
     }
+
+    static void DisplayWelcome () {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName (string username) {
+        Console.WriteLine("What what you like your username to be ?");
+        username = string. Console.ReadLine();
+        return username;
+    }
+
+    PromptUserNumber
+
+    SquareNumber
+
+    DisplayResult
+
 }
