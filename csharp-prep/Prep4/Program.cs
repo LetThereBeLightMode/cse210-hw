0051d670a987e85646efccab2145c3f3d0754f10
LetThereBeLightMode <wel24010@byui.edu>
2024-09-25 13:24:43 -0700

    Refactor program to include user input for a list of numbers and calculate total, average, and maximum

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,14 +7,26 @@ class Program
     static void Main(string[] args)
     {
         List<int> numbers = new List<int>();
-    int number; 
+
     Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+    int number = 0;
         do 
         {
+            int i = 0;
             numbers.Add(number);
-            Console.WriteLine("Enter another number:");
+            
+            if (i != 0); 
+            {
+                Console.WriteLine("Enter another number:");
+            }
             number = int.Parse(Console.ReadLine());
         }
         while (number != 0);
+    
+    Console.WriteLine($"Your total is: {numbers.Sum()}");
+    Console.WriteLine($"Your average is: {numbers.Average()}");
+    Console.WriteLine($"Your Max/Largest is: {numbers.Max()}");
+
+
     }
 }
