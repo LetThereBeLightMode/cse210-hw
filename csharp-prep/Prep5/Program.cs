a42b02e996ecc517b877423693a93a90608af39d
LetThereBeLightMode <wel24010@byui.edu>
2024-09-25 18:38:36 -0700

    Refactor program to include user input for username, prompt for a number, square the number, and display the result

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,26 +1,46 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string username = PromptUserName();
+        int usernum = PromptUserNumber();
+        SquareNumber(usernum);
+        DisplayResult(usernum, username);
     }
 
-    static void DisplayWelcome () {
+    static void DisplayWelcome () 
+    {
         Console.WriteLine("Welcome to the program!");
     }
 
-    static string PromptUserName (string username) {
-        Console.WriteLine("What what you like your username to be ?");
-        username = string. Console.ReadLine();
+    static string PromptUserName () 
+    {
+        Console.WriteLine("What what you like your username to be ?:");
+        string username = Console.ReadLine();
         return username;
     }
 
-    PromptUserNumber
+    static int PromptUserNumber ()
+    {
 
-    SquareNumber
+        Console.WriteLine("What number would you like squared today?:");
+        int usernumber = int.Parse(Console.ReadLine());
+        return usernumber;
+    }
 
-    DisplayResult
+    static int SquareNumber(int usernumber)
+    {
+        int numbersquared = (int)Math.Pow(usernumber, 2); 
+        return numbersquared;
+    }
+
+    static void DisplayResult(int number, string username)
+    {
+        Console.WriteLine($"{username}, your number squared is {number}");
+    }
 
 }
