fd16c9490fa1de9981d12ed7bca646f0a7e03963
LetThereBeLightMode <wel24010@byui.edu>
2024-09-26 06:52:45 -0700

    Refactor program to include Job class for managing job details

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+
+using System.Runtime.InteropServices;
+
+public class Job
+{
+   public string _jobTitle;
+   public int _totalcompensation;
+   public string _Company;
+
+   public int _startYear;
+
+   public int _endYear;
+
+
+
+
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job;
+        
+        job1._jobTitle="Super Senior Manager";
+        job1._Company="Beeg Business";
+        job1._startYear = 2024; 
+        
+        Job job2 = new Job;
+
+        
+        "
+
     }
 }
